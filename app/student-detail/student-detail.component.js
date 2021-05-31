e2ee78b68b911b73b4e758c134bf1571c83457b5
nskshared <nskshared@gmail.com>
2021-05-31 16:10:03 -0500

    forms

--- app/student-detail/student-detail.component.js
+++ app/student-detail/student-detail.component.js
@@ -5,12 +5,21 @@ angular.
   module('studentDetail').
   component('studentDetail', {
     templateUrl: 'student-detail/student-detail.template.html',
-    controller: ['$routeParams', 'Student',
-      function PhoneDetailController($routeParams, Student) {
+    controller: ['$routeParams', '$scope', 'Student',
+      function PhoneDetailController($routeParams, $scope, Student) {
         var self = this;
+        $scope.updatedStudent = {};
         self.student = Student.get({studentId: $routeParams.studentId}, function(student) {
           
         });
+
+        $scope.reset = function(student) {
+          $scope.updatedStudent = {};
+        };
+
+        $scope.update = function(student) {
+          $scope.updatedStudent = angular.copy(student);
+        };
       }
     ]
   });
